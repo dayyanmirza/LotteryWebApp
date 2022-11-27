589d8c5cf5007a1b76f9e918d4f3ebaba6044480
dayyanmirza <dayyanmirza15@gmail.com>
2022-11-27 13:12:39 +0000

    Added some input validation for name

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import Email
+from wtforms.validators import ValidationError
+
+
+def character_check(form, field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
+
+    email = EmailField(validators=[Email()])
+    firstname = StringField(validators=[character_check])
+    lastname = StringField(validators=[character_check])
     phone = StringField()
     password = PasswordField()
     confirm_password = PasswordField()
